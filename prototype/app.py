d158d8ac31351be30ba322a4c4ade272a09657d2
millingw <m_illingworth@hotmail.com>
2017-10-19 16:46:02 +0100

    Safety commit
    Added provisional code for handling incoming s3 authorization header; started work on encrypted token to pass to dynafed

--- prototype/app.py
+++ prototype/app.py
@@ -4,10 +4,13 @@ from flask_restful import Resource, Api
 import requests
 import xml.etree.cElementTree as xml
 from collections import namedtuple
+import sys, os, base64, datetime, hashlib, hmac 
+from Crypto.Cipher import AES
+
+from datetime import datetime
 
 from settings import BASE_DYNAFED_URL
-from settings import USERNAME, USERPASS
-
+from settings import ID_TO_KEY, ID_TO_ROLES, AUTH_TOKEN_NAME
 
 
 
@@ -39,6 +42,18 @@ def elem2file(elem):
     )
 
 
+def sign(key, msg):
+    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
+
+
+def getSignatureKey(key, dateStamp, regionName, serviceName):
+    kDate = sign(('AWS4' + key).encode('utf-8'), dateStamp)
+    kRegion = sign(kDate, regionName)
+    kService = sign(kRegion, serviceName)
+    kSigning = sign(kService, 'aws4_request')
+    return kSigning
+
+
 def list_directory_as_tuples(path):
 
        # dynafed will only let us descend one directory level at a time,
@@ -55,11 +70,130 @@ def list_directory_as_tuples(path):
        return results, r.status_code
 
 
+
+
+# extract the authorization information
+# and return all relevant information as a dict
+def process_authorization_header(authorization):
+    
+    split = authorization.split()
+    algorithm = None
+    credential = None
+    requestSignedHeaders = None
+    signature = None
+
+    for s in split:
+          if s == "AWS4-HMAC-SHA256":
+                algorithm = 'AWS4-HMAC-SHA256'
+          elif 'Credential' in s:
+                split1 = s.split('=')
+                credential = split1[1]
+          elif 'SignedHeaders' in s:
+                split1 = s.split('=')
+                requestSignedHeaders = split1[1]
+          elif 'Signature' in s:
+                split1 = s.split('=')
+                signature = split1[1]
+    
+    credential_split = credential.split('/')
+    
+    identity = None
+    date = None
+    region = None
+    service = None
+
+    if len(credential_split) > 0:
+          identity = credential_split[0]
+          date = credential_split[1]
+          region = credential_split[2]
+          service = credential_split[3]
+
+    result = {}
+    result["algorithm"] = algorithm
+    result["credential"] = credential
+    result["signed_headers"] = requestSignedHeaders
+    result["signature"] = signature
+    result["identity"] = identity
+    result["date"] = date
+    result["region"] = region
+    result["service"] = service
+
+    return result
+ 
+
+def get_required_headers(request):
+    
+    authorization = None
+    x_amz_date = None
+
+    try:
+       authorization = request.headers['Authorization']
+    except:
+       raise Exception("Authorization header not found")
+
+    try:
+       x_amz_date = request.headers['X-Amz-Date']
+    except:
+       raise Exception("x-amz-date header not found")
+    
+    return authorization, x_amz_date
+
+
+def validate_signature(user_key, processed_header):
+   # regenerate the key for the signature checking
+    sig_key = getSignatureKey(user_key, processed_header["date"], processed_header["region"], processed_header["service"])
+    
+    # todo - regenerate the signature and check against supplied signature
+    # todo - check timestamp is valid
+
+    
+
+
 @app.route('/', methods=['GET'])
-def handle_list_buckets():
+def handle_list_all_my_buckets():
 # list all the 'buckets' available to us and return them as a ListAllBuckets request
-    results, status_code = list_directory_as_tuples(BASE_DYNAFED_URL)
-    print results, status_code
+ 
+    authorization = None
+    x_amz_date = None
+
+    try:
+       authorization, x_amz_date = get_required_headers(request)
+    except Exception, e:
+       return str(e), 400
+
+    processed_header = None
+
+    try:
+       processed_header = process_authorization_header(authorization)
+    except:
+       return "Could not process authorization header", 500
+   
+    # should now have the user's identity, look them up in our map
+    identity = processed_header.get("identity")
+    user_key = ID_TO_KEY.get(identity)
+    if user_key == None:
+       return "No key found for identity", 403
+
+    validate_signature(user_key, processed_header)
+    
+    # get the user's roles
+    user_roles = ID_TO_ROLES.get(identity)
+    if user_roles == None:
+        return "No roles found", 403
+
+    timestamp = datetime.now()
+    timestamp_str = datetime.strftime(timestamp, '%a, %d %b %Y %H:%M:%S GMT')
+
+
+    # build the security token to be encrypted and sent in the query string
+    raw_token = identity + "/" + timestamp_str + "/" + user_roles
+
+    # todo - encrypt the token
+    encrypted_token = raw_token
+
+
+    results, status_code = list_directory_as_tuples(BASE_DYNAFED_URL + "?" + AUTH_TOKEN_NAME + "=" + encrypted_token)
+
     response = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
     response = response + "<ListAllMyBucketsResult xmlns=\"http://s3.amazonaws.com/doc/2006-03-01/\">"
     response = response + "<Buckets>"
@@ -76,32 +210,61 @@ def handle_list_buckets():
 @app.route('/<path:entity>', methods=['GET'])
 def handle_s3_request(entity):
 
+
+    authorization = None
+    x_amz_date = None
+
+    try:
+       authorization, x_amz_date = get_required_headers(request)
+    except Exception, e:
+       return e.message(), 400
+
+    processed_header = None
+
+    try:
+       processed_header = process_authorization_header(authorization)
+    except:
+       return "Could not process authorization header", 500
+   
+    # should now have the user's identity, look them up in our map
+    identity = processed_header.get("identity")
+    user_key = ID_TO_KEY.get(identity)
+    if user_key == None:
+       return "No key found for identity", 403
+
+    validate_signature(user_key, processed_header)
+    
+    # get the user's roles
+    user_roles = ID_TO_ROLES.get(identity)
+    if user_roles == None:
+        return "No roles found", 403
+
+    timestamp = datetime.now()
+    timestamp_str = datetime.strftime(timestamp, '%a, %d %b %Y %H:%M:%S GMT')
+
+
+    # build the security token to be encrypted and sent in the query string
+    raw_token = identity + "/" + timestamp_str + "/" + user_roles
+
+    # TODO: encrypt the token
+    encrypted_token = raw_token
+
     prefix = request.args.get('prefix')
     delimiter = request.args.get('delimiter')
     list_type = request.args.get('list-type')
 
     # if list_type not specified, assume we are looking for an object
     if list_type is None:
-
-          # TODO: authenticate user from request url
-          # TODO: encrypt token into url and sign url
-
-          response = requests.head(BASE_DYNAFED_URL + entity)
-          print response.status_code
-          if response.status_code != 302:
-		abort(response.status_code)
-          dynafed_key_location = response.headers['location']
-          return redirect(dynafed_key_location, 302)
+          return redirect(BASE_DYNAFED_URL + entity + "?" + AUTH_TOKEN_NAME + "=" + encrypted_token, 302)
 
 
     # AWS documentation says list-type must be '2'
     if list_type != "2":
 	return Response("Invalid list-type argument, must be 2", 500)
 
-    
 
     # naive version of code, does not handle prefix or delimiter
-    results, status_code = list_directory_as_tuples(BASE_DYNAFED_URL + entity)
+    results, status_code = list_directory_as_tuples(BASE_DYNAFED_URL + entity + "?" + AUTH_TOKEN_NAME + "=" + encrypted_token)
   
     if status_code != 207:
 	return Response("Error calling remote system", status_code)
